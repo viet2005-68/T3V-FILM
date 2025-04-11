47df485ab52d0778bf0504d3fcd467c87d0446ab
viet2005-68 <23020170@vnu.edu.vn>
2025-04-11 21:55:10 +0700

    create NavBar

---
+++ FrontEnd/src/components/navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import { ArrowDropDown, Notifications, Search } from "@mui/icons-material";
+import "./navbar.scss";
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../authContext/AuthContext";
+import { logout } from "../../authContext/AuthActions";
+
+export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const { dispatch } = useContext(AuthContext);
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+  return (
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+      <div className="container">
+        <div className="left">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1200px-Netflix_2015_logo.svg.png" />
+          <Link to="/" className="link">
+            <span>Homepage</span>
+          </Link>
+          <Link to="/series" className="link">
+            <span>Series</span>
+          </Link>
+          <Link to="/movies" className="link">
+            <span>Movies</span>
+          </Link>
+          <span>New and Popular</span>
+          <span>My list</span>
+        </div>
+        <div className="right">
+          <Search className="icon" />
+          <span>KID</span>
+          <Notifications className="icon" />
+          <img src={JSON.parse(localStorage.getItem("user")).profilePic} />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span onClick={() => dispatch(logout())}>Logout</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
