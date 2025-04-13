10cc08d71d4aaaed76e71192249e4cb32e896bea
tranthanhvan235 <98211651+tranthanhvan235@users.noreply.github.com>
2025-04-13 18:53:57 +0700

    edit filter component

--- FrontEnd/src/components/filter/Filter.jsx
+++ FrontEnd/src/components/filter/Filter.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
-import { MdTune } from "react-icons/md";
+import {useState} from "react";
+import {MdTune} from "react-icons/md";
+import {Button} from '@mui/material';
 import "./filter.scss";
 
 const filterOptions = {
@@ -8,18 +9,15 @@ const filterOptions = {
     year: ["Tất cả", "2024", "2023", "2022"],
 };
 
-export default function Filter({ onFilterApply }) {
+export default function Filter({onFilterApply}) {
     const [filters, setFilters] = useState({
-        country: "Tất cả",
-        genre: "Tất cả",
-        year: "Tất cả",
+        country: "Tất cả", genre: "Tất cả", year: "Tất cả",
     });
     const [showFilters, setShowFilters] = useState(false);
 
     const handleFilterChange = (type, value) => {
         setFilters((prev) => ({
-            ...prev,
-            [type]: value,
+            ...prev, [type]: value,
         }));
     };
 
@@ -29,40 +27,44 @@ export default function Filter({ onFilterApply }) {
         }
     };
 
-    return (
-        <div className="filter">
-            <div className="filter-toggle" onClick={() => setShowFilters(!showFilters)}>
-                <MdTune size={20} />
-                <span>Bộ lọc</span>
-            </div>
+    return (<div className="filter">
+        <div className="filter-toggle" onClick={() => setShowFilters(!showFilters)}>
+            <MdTune size={20}/>
+            <span>Bộ lọc</span>
+        </div>
 
-            <div className={`filter-elements ${showFilters ? "show" : ""}`}>
-                {Object.entries(filterOptions).map(([type, options]) => (
-                    <div className="fe-row" key={type}>
-                        <div className="fe-name">
-                            {type.charAt(0).toUpperCase() + type.slice(1)}:
-                        </div>
-                        <div className="fe-results">
-                            {options.map((item) => (
-                                <div
-                                    key={item}
-                                    className={`item ${filters[type] === item ? "active" : ""}`}
-                                    onClick={() => handleFilterChange(type, item)}
-                                >
-                                    {item}
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                ))}
-
-                <div style={{textAlign: "right", marginTop: "1rem"}}>
-                    <button className="btn-apply" onClick={handleApply}>Lọc</button>
-                    <button onClick={() => setFilters({country: "Tất cả", genre: "Tất cả", year: "Tất cả"})}>Reset
-                    </button>
-
+        <div className={`filter-elements ${showFilters ? "show" : ""}`}>
+            {Object.entries(filterOptions).map(([type, options]) => (<div className="fe-row" key={type}>
+                <div className="fe-name">
+                    {type.charAt(0).toUpperCase() + type.slice(1)}:
                 </div>
+                <div className="fe-results">
+                    {options.map((item) => (<div
+                        key={item}
+                        className={`item ${filters[type] === item ? "active-btn" : ""}`}
+                        onClick={() => handleFilterChange(type, item)}
+                    >
+                        {item}
+                    </div>))}
+                </div>
+            </div>))}
+
+            <div style={{textAlign: "right", marginTop: "1rem"}}>
+                <Button className='filter-btn'
+                    variant="contained"
+                    onClick={handleApply}
+                    sx={{mr: 1}}
+                >
+                    Lọc
+                </Button>
+
+                <Button className='reset-btn'
+                    variant="outlined"
+                    onClick={() => setFilters({country: "Tất cả", genre: "Tất cả", year: "Tất cả"})}
+                >
+                    Reset
+                </Button>
             </div>
         </div>
-    );
+    </div>);
 }
