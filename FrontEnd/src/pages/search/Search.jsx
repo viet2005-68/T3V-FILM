c0e1bcb2315dacdc6a550c191248fcaad9882f04
tranthanhvan235 <98211651+tranthanhvan235@users.noreply.github.com>
2025-04-14 00:17:49 +0700

    fix bug

--- FrontEnd/src/pages/search/Search.jsx
+++ FrontEnd/src/pages/search/Search.jsx
@@ -78,7 +78,6 @@ export default function Search() {
     }, [searchQuery]);
 
     useEffect(() => {
-
         const getAllMovie = async () => {
             try {
                 const res = await axios.get("/api/movies", {
@@ -93,7 +92,6 @@ export default function Search() {
             }
         };
 
-        getRandomLists();
         getAllMovie();
     }, []);
     const handleChangePage = (event, newPage) => {
