eb69d3bb33b408249d6810bcd56677c333f6dff2
thanhvinh0702 <thanhvinhhero0702@gmail.com>
2025-04-16 15:19:52 +0700

    Integrate third party's API to Admin add film function

--- admin/src/pages/product/Product.jsx
+++ admin/src/pages/product/Product.jsx
@@ -24,12 +24,6 @@ export default function Product() {
 
     return (
         <div className="product">
-            <div className="productTitleContainer">
-                <h1 className="productTitle">Movie</h1>
-                <Link to="/newProduct">
-                    <button className="productAddButton">Create</button>
-                </Link>
-            </div>
             <div className="productTop">
                 <div className="productTopRight">
                     <div className="productInfoTop">
